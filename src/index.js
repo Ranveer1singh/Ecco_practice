bf35d863a695e593aec943e18287379d437e5f02
Ranveer1singh <ranveersinghtomar2000@gmail.com>
2024-05-31 11:40:22 +0530

    server is running

--- src/index.js
+++ src/index.js
@@ -25,11 +25,7 @@ app.use('/api/admin',adminRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const server = http.createServer(app);
 // Start server
 server.listen(PORT, (err) => {
   if (err) {
