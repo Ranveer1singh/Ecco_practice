eb58644abc92ac73a0cc224c7f4b4e7a462bb6ee
Ran1veer <ranveer@matriyeacademy.com>
2024-06-20 10:32:34 +0530

    userschema

--- src/models/User.js
+++ src/models/User.js
@@ -36,6 +36,9 @@ userSchema.pre("save", async function (next) {
   }
 });
 
+userSchema.methods.IsPasswordCorrect = async function(password){
+  return await bcrypt.compare(password,this/password);
+}
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
